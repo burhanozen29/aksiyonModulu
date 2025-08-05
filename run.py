d4f1020d040358df1e76eb7c4fb55dabdd9fc548
Burhan ÖZEN <burhan.ozen@outlook.com>
2025-08-05 15:58:18 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,4 +8,5 @@ Created on Tue Aug  5 14:26:39 2025
 import os
 
 # Streamlit ana uygulamasını başlat
-os.system("streamlit run aksiyon_kpi_modulu/ui_main.py")
+
+os.system("streamlit run aksiyon_kpi_modulu/Anasayfa.py")
