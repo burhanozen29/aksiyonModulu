c7d5a3ddacfad64b58d26f693a20093188af152e
Burhan ÖZEN <burhan.ozen@outlook.com>
2025-08-08 16:14:43 +0300

    Update auth.py

--- aksiyon_kpi_modulu/auth.py
+++ aksiyon_kpi_modulu/auth.py
@@ -12,6 +12,9 @@ def kullanici_dogrula(kullanici_adi, sifre):
     if user:
         birimler = user.get("Birim", "")
         rol = user.get("Rol", "")
+        bagli_birim = user.get("Bağlı Kişi Birim","")
+        bagli_kisi = user.get("Bağlı Kişi","")
+        isim = user.get("İsim","")
         birim_listesi = [b.strip() for b in birimler.split(",")] if isinstance(birimler, str) else []
-        return {"birimler": birim_listesi, "rol": rol}
+        return {"birimler": birim_listesi, "rol": rol, "bagli_birim":bagli_birim, "bagli_kisi":bagli_kisi,"isim":isim}
     return None
